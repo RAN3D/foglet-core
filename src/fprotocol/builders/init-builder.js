b700b7ec95cbc78af7641b9c9a122797f933e356
Callidon <tminier01@gmail.com>
2017-07-26 09:55:30 +0200

    add missing file :3

---
+++ src/fprotocol/builders/init-builder.js
@@ -0,0 +1,49 @@
+/*
+MIT License
+
+Copyright (c) 2016-2017 Grall Arnaud
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+'use strict';
+
+/**
+ * An InitBuilder build the _init method for a protocol prototype.
+ * @author Thomas Minier
+ */
+class InitBuilder {
+  /**
+   * Constructor
+   * @param  {function} callback - The callback executed by the _init method
+   */
+  constructor (callback) {
+    this._callback = callback;
+  }
+
+  /**
+   * Apply the builder on a protocol subclass to build the _init method in it.
+   * @param  {function} protocol - The protocol class
+   * @return {void}
+   */
+  apply (protocol) {
+    protocol.prototype._init = this._callback;
+  }
+}
+
+module.exports = InitBuilder;
