bfbbc5474607c7e97416bbf7941290acc31dd66d
Grall Arnaud <folkvir@outlook.fr>
2017-10-13 14:10:56 +0200

    externalize Communication Class for creation of internal communication channels in other network (see ran3d/foglet-overlay for an example.) (need a release)

--- foglet-core.js
+++ foglet-core.js
@@ -24,6 +24,7 @@ SOFTWARE.
 'use strict';
 
 const Foglet = require('./src/foglet.js');
+const Communication = require('./src/network/communication/communication.js');
 const protocol = require('./src/fprotocol/protocol-builder.js');
 // abstracts
 const AbstractNetwork = require('./src/network/abstract/abstract-network.js');
@@ -33,6 +34,13 @@ const TManOverlay = require('./src/network/abstract/tman-overlay.js');
 module.exports = {
   Foglet,
   protocol,
+  /*
+   Externalize Communication Class to enable the creation of a specific communication channel
+   inside the creation of a new network: 
+   eg: create your own overlay or rps and use our communication channel for internal communications
+   Warning: use a unique protocol
+  */
+  communication: Communication, 
   abstract: {
     rps: AbstractNetwork,
     overlay: AbstractOverlay,
