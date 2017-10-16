a7a6104b34febaf51f430bc3e3267467560deb87
folkvir <dev.arnaudgrall@gmail.com>
2017-10-16 16:45:37 +0200

    update packages, and enhance tests

--- tests/foglet-main-test.js
+++ tests/foglet-main-test.js
@@ -1,14 +1,15 @@
 'use strict';
-
+// const assert = require('chai').assert;
 const Foglet = require('../src/foglet.js');
 const utils = require('./utils.js');
 
 describe('[FOGLET] Other functions tests', function () {
+  this.timeout(20000);
   it('[FOGLET] getRandomNeighbourId is in getNeighbours', function (done) {
-    let foglets = utils.buildFog(Foglet, 2);
-    let f1 = foglets[0], f2 = foglets[1];
+    const foglets = utils.buildFog(Foglet, 2);
+    const f1 = foglets[0], f2 = foglets[1];
 
-    utils.pathConnect(foglets).then(() => {
+    utils.pathConnect(foglets, 2000).then(() => {
       console.log('Peers: ', f1.getNeighbours(), f2.getNeighbours());
       console.log('Random:', f1.getRandomNeighbourId(), f2.getRandomNeighbourId());
       assert.include(f1.getNeighbours(), f1.getRandomNeighbourId());
