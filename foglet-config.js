14d55202c9f035999c5203c2b8560046ecdca9cf
folkvir <dev.arnaudgrall@gmail.com>
2017-11-16 17:20:31 +0100

    move to foglet-config.js build file

---
+++ foglet-config.js
@@ -0,0 +1,34 @@
+'use strict';
+const path = require('path');
+module.exports = {
+  browsers: [ 'Firefox' ],
+  timeout: 20000,
+  lint: true,
+  build: {
+    entry: './foglet-core.js',
+    output: {
+      'path': path.resolve(process.cwd(), 'dist'),
+      'filename': 'foglet.bundle.js',
+      'library': 'foglet',
+      'libraryTarget': 'umd',
+      'umdNamedDefine': true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: () => {
+            return true;
+          },
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [ 'env' ]
+            }
+          }
+        }
+      ]
+    },
+    devtool: 'source-map'
+  }
+};
