c840f1023dfa500ce825b0934bb11b4109352e69
Folkvir <folkvir@outlook.fr>
2018-05-26 17:34:00 +0200

    move build configs into a specific folder

---
+++ configs/foglet-webpack-config-min.js
@@ -0,0 +1,12 @@
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const lmerge = require('lodash.merge')
+const webpackconfig = require('./foglet-webpack-config')
+module.exports = lmerge(webpackconfig, {
+  mode: 'production',
+  output: {
+    'filename': 'foglet.bundle.min.js'
+  },
+  plugins: [new UglifyJSPlugin({
+    sourceMap: true
+  })]
+})
