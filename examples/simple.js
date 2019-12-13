d468d56e4729405dff6f74babc94cdde01d09f0d
Grall Arnaud <folkvir@outlook.fr>
2019-12-13 16:59:26 +0100

    multiple layers, networks incoming

--- examples/simple.js
+++ examples/simple.js
@@ -1,5 +1,30 @@
-const Core = require('../foglet-core')
+console.log(require('../foglet-core'))
+const { Core } = require('../foglet-core')
+const { FullConnected } = require('../foglet-core').Networks
+const { LocalLayer } = require('../foglet-core').Layers
 
-const a = new Core()
+const a = new Core('peer:a')
+const anet = new FullConnected('full', a.options)
+a.manager.addLayer(new LocalLayer('local', a.options))
 console.log(a.options.serialize())
-console.log(a)
+
+const b = new Core('peer:b')
+const bnet = new FullConnected('full', b.options)
+b.manager.addLayer(new LocalLayer('local', b.options))
+console.log(b.options.serialize())
+
+async function main () {
+  await a.manager.connect()
+  await b.manager.connect()
+}
+
+main().then(async () => {
+  console.log('connected')
+  console.log('A:', a.manager.neighbours)
+  console.log('B:', b.manager.neighbours)
+
+  // create the network managing A
+
+}).catch(e => {
+  console.error(e)
+})
--- foglet-core.js
+++ foglet-core.js
@@ -1,1 +1,9 @@
-module.exports = require('./lib/core')
+module.exports = {
+  Core: require('./lib/core'),
+  Networks: require('./lib/networks'),
+  Manager: require('./lib/manager'),
+  View: require('./lib/view'),
+  Utils: require('./lib/utils'),
+  Asbtracts: require('./lib/abstracts'),
+  Layers: require('./lib/layers')
+}
