cadca0214072fc9693a786d39a886647a71b3568
Grall Arnaud <folkvir@outlook.fr>
2020-08-28 12:39:09 +0200

    working on networks

--- tests/networks/a-to-b-test.js
+++ tests/networks/a-to-b-test.js
@@ -3,7 +3,7 @@ const { Core } = require('../../foglet-core')
 const { FullConnected } = require('../../foglet-core').Networks
 const { LocalLayer } = require('../../foglet-core').Layers
 
-test('(full-connected) all peers should have 99 neighbours on both networks', t => {
+test('(full-connected) should be connected correctly in a bidirectional for 2 peers', t => {
   const peers = []
   const protocol = 'protocol-test'
   function createPeer (id) {
@@ -11,31 +11,27 @@ test('(full-connected) all peers should have 99 neighbours on both networks', t 
     peer.on('data', (...args) => console.log('[%s] receive: ', peer.id, ...args))
     peer.manager.setLayer(new LocalLayer('local', peer.options))
     peer.manager.addNetwork(new FullConnected(protocol, peer.options))
-    // peer.manager.addNetwork(new FullConnected('full2', peer.options))
     peers.push(peer)
     return peer
   }
 
-  const max = 3
+  const max = 2
   for (let i = 0; i < max; i++) {
     createPeer('peer:' + i)
   }
 
   async function main () {
-    return peers.reduce((acc, cur, index) => acc.then(async () => {
-      return cur.join()
-    }), Promise.resolve())
+    await peers[0].join()
+    await peers[1].join()
   }
 
   return main().then(async () => {
-    return new Promise((resolve, reject) => {
-      peers.forEach(peer => {
-        t.assert(peer.manager.view.size() === max - 1)
-        peer.manager.view.view.forEach((networkNames) => {
-          t.deepEqual(networkNames[protocol], max - 1)
-        })
+    peers.forEach(peer => {
+      peer.manager.view.view.forEach((obj, id) => {
+        t.assert(obj[protocol] === 1)
+        t.assert(peer.manager.view.size() === 1)
+        t.assert(peer.manager.view.has(peer.manager.networks[0], (peer.id === peers[0].id ? 'peer:1' : 'peer:0')))
       })
-      resolve()
     })
   }).catch(e => {
     console.error(e)
