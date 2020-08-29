6271e853db0a9baaff7ad5d09cd49209a1d6e724
Grall Arnaud <folkvir@outlook.fr>
2020-08-29 15:45:31 +0200

    fullconnected network implemented

--- tests/networks/a-to-b-test.js
+++ tests/networks/a-to-b-test.js
@@ -1,3 +1,4 @@
+/* eslint no-async-promise-executor: off */
 const test = require('ava')
 const { Core } = require('../../foglet-core')
 const { FullConnected } = require('../../foglet-core').Networks
@@ -26,13 +27,22 @@ test('(full-connected) should be connected correctly in a bidirectional for 2 pe
   }
 
   return main().then(async () => {
-    peers.forEach(peer => {
-      peer.manager.view.view.forEach((obj, id) => {
-        t.assert(obj[protocol] === 1)
-        t.assert(peer.manager.view.size() === 1)
-        t.assert(peer.manager.view.has(peer.manager.networks[0], (peer.id === peers[0].id ? 'peer:1' : 'peer:0')))
-      })
-    })
+    return Promise.all(peers.map(peer => (async () => {
+      const values = [...peer.manager.view.view]
+      t.assert(values.length === 1)
+      for (let i = 0; i < values.length; ++i) {
+        const id = values[i][0]
+        const protocols = values[i][1]
+        t.assert(protocols[protocol] === 1)
+        t.deepEqual(id, (peer.id === 'peer:0' ? 'peer:1' : 'peer:0'))
+        // get the view by locking it
+        const { lock, view } = await peer.manager.networks[0].getView()
+        t.assert(lock !== undefined)
+        t.deepEqual(view.length, 1, 'the view should have one peer')
+        // unlock the view without modification
+        await peer.manager.networks[0].mergeView(lock)
+      }
+    })()))
   }).catch(e => {
     console.error(e)
     throw e
--- tests/networks/full-connected-test.js
+++ tests/networks/full-connected-test.js
@@ -1,9 +1,11 @@
+/* eslint no-async-promise-executor: off */
 const test = require('ava')
 const { Core } = require('../../foglet-core')
 const { FullConnected } = require('../../foglet-core').Networks
 const { LocalLayer } = require('../../foglet-core').Layers
 
-test('(full-connected) all peers should have 99 neighbours on both networks', t => {
+const max = 20
+test(`(full-connected) all peers should have ${max - 1} neighbours on both networks`, t => {
   const peers = []
   const protocol = 'protocol-test'
   function createPeer (id) {
@@ -16,7 +18,6 @@ test('(full-connected) all peers should have 99 neighbours on both networks', t 
     return peer
   }
 
-  const max = 3
   for (let i = 0; i < max; i++) {
     createPeer('peer:' + i)
   }
@@ -28,15 +29,22 @@ test('(full-connected) all peers should have 99 neighbours on both networks', t 
   }
 
   return main().then(async () => {
-    return new Promise((resolve, reject) => {
-      peers.forEach(peer => {
-        t.assert(peer.manager.view.size() === max - 1)
-        peer.manager.view.view.forEach((networkNames) => {
-          t.deepEqual(networkNames[protocol], max - 1)
-        })
-      })
-      resolve()
-    })
+    return Promise.all(peers.map(peer => (async () => {
+      const values = [...peer.manager.view.view]
+      // console.log('[%s] View: ', peer.id, values)
+      t.deepEqual(values.length, max - 1, `should have max-1=${max - 1} peers in our view`)
+      for (let i = 0; i < values.length; ++i) {
+        // const id = values[i][0]
+        const protocols = values[i][1]
+        t.deepEqual(protocols[protocol], 1, 'should have 1 on each protocol')
+        // get the view by locking it
+        const { lock, view } = await peer.manager.networks[0].getView()
+        t.assert(lock !== undefined)
+        t.deepEqual(view.length, max - 1, 'the view should have one peer')
+        // unlock the view without modification
+        await peer.manager.networks[0].mergeView(lock)
+      }
+    })()))
   }).catch(e => {
     console.error(e)
     throw e
